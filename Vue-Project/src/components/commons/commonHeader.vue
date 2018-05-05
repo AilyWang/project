104a67e61330838e4fac4b6134e857dda88865eb
AilyWang <973643397@qq.com>
2018-05-05 14:42:03 +0800

    修改小图标文件夹

---
+++ Vue-Project/src/components/commons/commonHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="header">
+        
+    </div>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style scoped>
+    *{
+        box-sizing: border-box;
+    }
+    body,div,p,ul,ol,li,dl,dt,dd,table,tr,td,form,hr,fieldset,h1,h2,h3,h4,h5,h6,img,input{
+        margin: 0;
+        padding: 0;
+    }
+    html{
+        font-size: 26.67vw;
+    }
+    html body{
+        height: 100%;
+    }
+    body{
+        font-family: "微软雅黑",Arial;
+        background: #f7f7f8;
+    }
+    a{
+        text-decoration:none;
+        color: #333;
+    }
+    ul,ol{
+        list-style:none;
+    }
+    img{
+        border: 0;
+        display: block;
+    }
+    .header{
+        width: 100%;
+        height: 50px;
+        background: #3c8dbc;
+    }
+</style>
